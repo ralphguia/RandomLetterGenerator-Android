a4885092642b85b0116ce5d47f09f028bf7841dc
Ralph G <ralph.christian.guia@gmail.com>
2022-03-13 22:56:32 +0100

    Adding Support for the Up Button

--- app/src/main/java/com/example/randomlettergenerator/MainActivity.kt
+++ app/src/main/java/com/example/randomlettergenerator/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.randomlettergenerator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.randomlettergenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
